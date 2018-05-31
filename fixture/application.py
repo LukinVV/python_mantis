f7a348c0c3977f690aba2fb872e3d1027075689d
gramb <Lukinvv@inbox.ru>
2018-05-31 22:58:21 +0300

    тест на проверку логина

---
+++ fixture/application.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "chrome":
+            self.wd = webdriver.Chrome(
+                "C:\Windows\SysWOW64\chromedriver.exe")  # "C:\Windows\SysWOW64\chromedriver.exe" - может пригодится
+        elif browser == "firefox":
+            self.wd = webdriver.Firefox(capabilities={"marionette": False})
+        elif browser == "edge":
+            self.wd = webdriver.Edge()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError('Unrecognized browser %s' % browser)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
