13a1bdb9f1c5228c5f7d0199c9374bc474bed86c
gramb <Lukinvv@inbox.ru>
2018-06-02 22:51:08 +0300

    отключаем капчу
    я не робот

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 import json
 import os.path
 import jsonpickle
-import subprocess
+import ftputil
 from fixture.application import Application
 from fixture.orm import ORMFixture
 
@@ -20,14 +20,14 @@ def load_config(file):
 
 
 @pytest.fixture()
-def app(request):
+def app(request, config):
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))['web']
-    web_admin_config = load_config(request.config.getoption("--target"))['webadmin']
+    # web_config = load_config(request.config.getoption("--target"))['web']
+    # web_admin_config = load_config(request.config.getoption("--target"))['webadmin']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=web_config["baseUrl"])
-    fixture.session.ensure_login(username=web_admin_config['user_name'], password=web_admin_config['password'])
+        fixture = Application(browser=browser, base_url=config["web"]["baseUrl"])
+    fixture.session.ensure_login(username=config['webadmin']['user_name'], password=config['webadmin']['password'])
     return fixture
 
 
@@ -65,8 +65,36 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture(scope="session")
-def orm(request):
-    orm_config = load_config(request.config.getoption("--target"))['db']
-    ormfixture = ORMFixture(host=orm_config['host'], name=orm_config['name'], user=orm_config['user'],
-                            password=orm_config['password'])
+def orm(request, config):
+    ormfixture = ORMFixture(host=config['db']['host'], name=config['db']['name'], user=config['db']['user'],
+                          password=config['db']['password'])
     return ormfixture
+
+
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    def fin():
+        restore_server_configurtion(config['ftp']['host'],config['ftp']['username'],config['ftp']['password'])
+    request.addfinalizer(fin)
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            remote.remove("config_inc.php.bak")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php","config_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__),"resources/config_inc.php"), "config_inc.php")
+
+
+def restore_server_configurtion(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.bak","config_inc.php")
